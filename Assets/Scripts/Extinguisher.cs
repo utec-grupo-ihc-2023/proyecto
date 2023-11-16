82e05437cb8b28ff0d17073904ab8e11f637b859
Alberto Oporto <otreblain@gmail.com>
2023-11-16 18:12:19 -0500

    chore(Udon): Port raycast to udon#

--- Assets/Scripts/Extinguisher.cs
+++ Assets/Scripts/Extinguisher.cs
@@ -14,6 +14,8 @@ public class Extinguisher : UdonSharpBehaviour
     public float maxDistance = 10.0f;
     public Transform foamOrigin;
 
+    bool sendRaycast = false;
+
     public override void InputUse(bool value, UdonInputEventArgs args)
     {
         // Check if the current object is held
@@ -23,10 +25,52 @@ public class Extinguisher : UdonSharpBehaviour
         }
 
         foam.SetActive(value);
+        sendRaycast = value;
     }
 
     void Start()
     {
         
+    }
+
+    bool IsCompatible(GameObject fire)
+    {
+        for(int i = 0; i < fireTypes.Length; i++)
+        {
+            if(fireTypes[i] == fire.name)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void Extinguish(GameObject fire)
+    {
+        if(IsCompatible(fire))
+        {
+            fire.SetActive(false);
+        }
+    }
+
+    void SendRaycast()
+    {
+        Ray ray = new Ray(foamOrigin.position, foamOrigin.TransformDirection(Vector3.right));
+
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance))
+        {
+            Debug.DrawRay(ray.origin, hitInfo.point);
+
+            Extinguish(hitInfo.transform.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if(sendRaycast)
+        {
+            SendRaycast();
+        }
     }
 }
