983cc60f8e76a8d76970e293951427a829ecb0c2
Alberto Oporto <otreblain@gmail.com>
2023-11-16 17:24:49 -0500

    feat(Udon): Add Extinguisher Udonsharp script

---
+++ Assets/Scripts/Extinguisher.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+public class Extinguisher : UdonSharpBehaviour
+{
+    public GameObject foam;
+    public VRCPickup pickup;
+    public string[] fireTypes;
+    public float maxDistance = 10.0f;
+    public Transform foamOrigin;
+
+    public override void InputUse(bool value, UdonInputEventArgs args)
+    {
+        // Check if the current object is held
+        if(!pickup.IsHeld) 
+        {
+            return;
+        }
+
+        foam.SetActive(value);
+    }
+
+    void Start()
+    {
+        
+    }
+}
